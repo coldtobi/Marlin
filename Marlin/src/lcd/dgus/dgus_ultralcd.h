64e0e2c577e38f2e042c18415d6342856a1b82c9
Scott Lahteine <github@thinkyhead.com>
2019-03-06 23:59:15 -0600

    Remove SPI-based LCD elements from DGUS

--- Marlin/src/lcd/dgus/dgus_ultralcd.h
+++ Marlin/src/lcd/dgus/dgus_ultralcd.h
@@ -39,87 +39,15 @@ public:
     progress_bar_percent = MAX(newprogress, 100);
   }
 
-  #if HAS_SPI_LCD
-    #if ENABLED(SHOW_BOOTSCREEN)
-      inline static void show_bootscreen() {}; // Handled by DGUSDisplay::InitDisplay()
-    #endif
-
-    #if ENABLED(FILAMENT_LCD_DISPLAY) && ENABLED(SDSUPPORT)
-      static millis_t next_filament_display;
-    #endif
-
-    static void quick_feedback(const bool clear_buttons=true);
+  #if ENABLED(SHOW_BOOTSCREEN)
+    inline static void show_bootscreen() {}; // Handled by DGUSDisplay::InitDisplay()
   #endif
 
-  #if HAS_LCD_MENU
-
-    #if ENABLED(SDSUPPORT)
-      #if ENABLED(SCROLL_LONG_FILENAMES)
-        static uint8_t filename_scroll_pos, filename_scroll_max;
-      #endif
-      static const char * scrolled_filename(CardReader &theCard, const uint8_t maxlen, uint8_t hash, const bool doScroll);
-    #endif
-
-    #if IS_KINEMATIC
-      static bool processing_manual_move;
-    #else
-      static constexpr bool processing_manual_move = false;
-    #endif
-
-    #if E_MANUAL > 1
-      static int8_t manual_move_e_index;
-    #else
-      static constexpr int8_t manual_move_e_index = 0;
-    #endif
-
-    static int16_t preheat_hotend_temp[2], preheat_bed_temp[2];
-    static uint8_t preheat_fan_speed[2];
-
-    static void manage_manual_move();
-
-    static bool lcd_clicked;
-    static bool use_click();
-
-    static void synchronize(PGM_P const msg=NULL);
-
-    static screenFunc_t currentScreen;
-    static void goto_screen(const screenFunc_t screen, const uint32_t encoder=0);
-    static void save_previous_screen();
-    static void goto_previous_screen();
-    static void return_to_status();
-    static inline bool on_status_screen() { return currentScreen == status_screen; }
-    static inline void run_current_screen() { (*currentScreen)(); }
-
-    #if ENABLED(LIGHTWEIGHT_UI)
-      static void lcd_in_status(const bool inStatus);
-    #endif
-
-    static inline void defer_status_screen(const bool defer) {
-      #if LCD_TIMEOUT_TO_STATUS
-        defer_return_to_status = defer;
-      #else
-        UNUSED(defer);
-      #endif
-    }
-
-    static inline void goto_previous_screen_no_defer() {
-      defer_status_screen(false);
-      goto_previous_screen();
-    }
-
-    #if ENABLED(SD_REPRINT_LAST_SELECTED_FILE)
-      static void reselect_last_file();
-    #endif
-
-    #if ENABLED(G26_MESH_VALIDATION)
-      static inline void chirp() { buzz(LCD_FEEDBACK_FREQUENCY_DURATION_MS, LCD_FEEDBACK_FREQUENCY_HZ); }
-    #endif
-
-    #if ENABLED(AUTO_BED_LEVELING_UBL)
-      static void ubl_plot(const uint8_t x, const uint8_t inverted_y);
-    #endif
-
+  #if ENABLED(FILAMENT_LCD_DISPLAY) && ENABLED(SDSUPPORT)
+    static millis_t next_filament_display;
   #endif
+
+  static void quick_feedback(const bool clear_buttons=true);
 
   static inline void update_buttons() {}
 
--- Marlin/src/lcd/dgus/ultralcd_dgus.cpp
+++ Marlin/src/lcd/dgus/ultralcd_dgus.cpp
@@ -33,8 +33,6 @@ MarlinUI_DGUS ui;
 
 #include "../../sd/cardreader.h"
 
-#if HAS_SPI_LCD   // likely also want some of those functions for DGUS displays, but TODO
-
 #include "../lcdprint.h"
 
 #include "../../sd/cardreader.h"
@@ -68,34 +66,12 @@ uint8_t MarlinUI_DGUS::progress_bar_percent = 0;
   uint8_t lcd_sd_status;
 #endif
 
-#if HAS_LCD_MENU && LCD_TIMEOUT_TO_STATUS
-  bool MarlinUI::defer_return_to_status;
-#endif
-
 #if ENABLED(FILAMENT_LCD_DISPLAY) && ENABLED(SDSUPPORT)
   millis_t MarlinUI::next_filament_display; // = 0
 #endif
 
 #if HAS_GRAPHICAL_LCD
   bool MarlinUI::drawing_screen, MarlinUI::first_page; // = false
-#endif
-
-#if HAS_LCD_MENU
-  #include "menu/menu.h"
-  #include "../sd/cardreader.h"
-
-  #if ENABLED(SDSUPPORT)
-    const char * MarlinUI::scrolled_filename(CardReader &theCard, const uint8_t maxlen, uint8_t hash, const bool doScroll) {
-      const char *outstr = theCard.longest_filename();
-      if (theCard.longFilename[0]) {
-        theCard.longFilename[maxlen] = '\0'; // cutoff at screen edge
-      }
-      return outstr;
-    }
-  #endif
-
-  screenFunc_t MarlinUI::currentScreen; // Initialized in CTOR
-
 #endif
 
 void MarlinUI_DGUS::init() {
@@ -105,10 +81,6 @@ void MarlinUI_DGUS::init() {
   #if ENABLED(SDSUPPORT) && PIN_EXISTS(SD_DETECT)
     SET_INPUT_PULLUP(SD_DETECT_PIN);
     lcd_sd_status = 2; // UNKNOWN
-  #endif
-
-  #if HAS_TRINAMIC && HAS_LCD_MENU
-    init_tmc_section();
   #endif
 }
 
@@ -142,78 +114,6 @@ void MarlinUI::quick_feedback(const bool clear_buttons/*=true*/) {
   UNUSED(clear_buttons);
   buzz(LCD_FEEDBACK_FREQUENCY_DURATION_MS, LCD_FEEDBACK_FREQUENCY_HZ);
 }
-
-////////////////////////////////////////////
-/////////////// Manual Move ////////////////
-////////////////////////////////////////////
-
-#if HAS_LCD_MENU
-
-  extern bool no_reentry; // Flag to prevent recursion into menu handlers
-
-  int8_t manual_move_axis = (int8_t)NO_AXIS;
-  millis_t manual_move_start_time = 0;
-
-  #if IS_KINEMATIC
-    bool MarlinUI::processing_manual_move = false;
-    float manual_move_offset = 0;
-  #endif
-
-  #if E_MANUAL > 1
-    int8_t MarlinUI::manual_move_e_index = 0;
-  #endif
-
-  /**
-   * If the most recent manual move hasn't been fed to the planner yet,
-   * and the planner can accept one, send a move immediately.
-   */
-  void MarlinUI::manage_manual_move() {
-
-    if (processing_manual_move) return;
-
-    if (manual_move_axis != (int8_t)NO_AXIS && ELAPSED(millis(), manual_move_start_time) && !planner.is_full()) {
-
-      #if IS_KINEMATIC
-
-        const float old_feedrate = feedrate_mm_s;
-        feedrate_mm_s = MMM_TO_MMS(manual_feedrate_mm_m[manual_move_axis]);
-
-        #if EXTRUDERS > 1
-          const int8_t old_extruder = active_extruder;
-          if (manual_move_axis == E_AXIS) active_extruder = manual_move_e_index;
-        #endif
-
-        // Set movement on a single axis
-        set_destination_from_current();
-        destination[manual_move_axis] += manual_move_offset;
-
-        // Reset for the next move
-        manual_move_offset = 0;
-        manual_move_axis = (int8_t)NO_AXIS;
-
-        // DELTA and SCARA machines use segmented moves, which could fill the planner during the call to
-        // move_to_destination. This will cause idle() to be called, which can then call this function while the
-        // previous invocation is being blocked. Modifications to manual_move_offset shouldn't be made while
-        // processing_manual_move is true or the planner will get out of sync.
-        processing_manual_move = true;
-        prepare_move_to_destination(); // will call set_current_from_destination()
-        processing_manual_move = false;
-
-        feedrate_mm_s = old_feedrate;
-        #if EXTRUDERS > 1
-          active_extruder = old_extruder;
-        #endif
-
-      #else
-
-        planner.buffer_line(current_position, MMM_TO_MMS(manual_feedrate_mm_m[manual_move_axis]), manual_move_axis == E_AXIS ? manual_move_e_index : active_extruder);
-        manual_move_axis = (int8_t)NO_AXIS;
-
-      #endif
-    }
-  }
-
-#endif // HAS_LCD_MENU
 
 /**
  * Update the LCD, read encoder buttons, etc.
@@ -256,40 +156,6 @@ void MarlinUI_DGUS::update() {
 
   static millis_t next_lcd_update_ms;
 
-  #if HAS_LCD_MENU
-
-    #if LCD_TIMEOUT_TO_STATUS
-      static millis_t return_to_status_ms = 0;
-    #endif
-
-    // Handle any queued Move Axis motion
-    manage_manual_move();
-
-    // Update button states for button_pressed(), etc.
-    // If the state changes the next update may be delayed 300-500ms.
-    update_buttons();
-
-    // If the action button is pressed...
-    static bool wait_for_unclick; // = 0
-    if (!external_control && button_pressed()) {
-      if (!wait_for_unclick) {                        // If not waiting for a debounce release:
-        wait_for_unclick = true;                      //  - Set debounce flag to ignore continous clicks
-        lcd_clicked = !wait_for_user && !no_reentry;  //  - Keep the click if not waiting for a user-click
-        wait_for_user = false;                        //  - Any click clears wait for user
-        quick_feedback();                             //  - Always make a click sound
-      }
-    }
-    else wait_for_unclick = false;
-
-    #if HAS_DIGITAL_BUTTONS && BUTTON_EXISTS(BACK)
-      if (LCD_BACK_CLICKED()) {
-        quick_feedback();
-        goto_previous_screen();
-      }
-    #endif
-
-  #endif // HAS_LCD_MENU
-
   #if ENABLED(SDSUPPORT) && PIN_EXISTS(SD_DETECT)
 
     const uint8_t sd_status = (uint8_t)IS_SD_INSERTED();
@@ -330,149 +196,136 @@ void MarlinUI_DGUS::update() {
       update_indicators();
     #endif
 
-    #if HAS_LCD_MENU && LCD_TIMEOUT_TO_STATUS
-      // Return to Status Screen after a timeout
-      if (on_status_screen() || defer_return_to_status)
-        return_to_status_ms = ms + LCD_TIMEOUT_TO_STATUS;
-      else if (ELAPSED(ms, return_to_status_ms))
-        return_to_status();
-    #endif
-
   ScreenHandler.loop();
 
   } // ELAPSED(ms, next_lcd_update_ms)
 }
 
-#endif // HAS_SPI_LCD
 
-#if HAS_SPI_LCD || ENABLED(EXTENSIBLE_UI)
+////////////////////////////////////////////
+/////////////// Status Line ////////////////
+////////////////////////////////////////////
 
-  ////////////////////////////////////////////
-  /////////////// Status Line ////////////////
-  ////////////////////////////////////////////
+void MarlinUI::finishstatus(const bool persist) {
 
-  void MarlinUI::finishstatus(const bool persist) {
+  #if !(ENABLED(LCD_PROGRESS_BAR) && (PROGRESS_MSG_EXPIRE > 0))
+    UNUSED(persist);
+  #endif
 
-    #if !(ENABLED(LCD_PROGRESS_BAR) && (PROGRESS_MSG_EXPIRE > 0))
-      UNUSED(persist);
+  #if ENABLED(LCD_PROGRESS_BAR)
+    progress_bar_ms = millis();
+    #if PROGRESS_MSG_EXPIRE > 0
+      expire_status_ms = persist ? 0 : progress_bar_ms + PROGRESS_MSG_EXPIRE;
     #endif
+  #endif
 
-    #if ENABLED(LCD_PROGRESS_BAR)
-      progress_bar_ms = millis();
-      #if PROGRESS_MSG_EXPIRE > 0
-        expire_status_ms = persist ? 0 : progress_bar_ms + PROGRESS_MSG_EXPIRE;
-      #endif
-    #endif
+  #if ENABLED(FILAMENT_LCD_DISPLAY) && ENABLED(SDSUPPORT)
+    next_filament_display = millis() + 5000UL; // Show status message for 5s
+  #endif
 
-    #if ENABLED(FILAMENT_LCD_DISPLAY) && ENABLED(SDSUPPORT)
-      next_filament_display = millis() + 5000UL; // Show status message for 5s
-    #endif
+  refresh();
+}
 
-    refresh();
-  }
-
-   bool MarlinUI_DGUS::status_set = false;
-   uint8_t MarlinUI_DGUS::status_message_level = 0;
+ bool MarlinUI_DGUS::status_set = false;
+ uint8_t MarlinUI_DGUS::status_message_level = 0;
 
 
-  void MarlinUI_DGUS::set_status(const char * const message, const bool persist) {
-    if (status_message_level > 0) return;
-    if (!message) return;
+void MarlinUI_DGUS::set_status(const char * const message, const bool persist) {
+  if (status_message_level > 0) return;
+  if (!message) return;
 
-    status_set = (message[0] == '\0' ? false : true);
+  status_set = (message[0] == '\0' ? false : true);
 
-    // The DGUS Display does not support UTF8, so we just pass it to it as it...
-    DGUS_VP_Variable ramcopy;
-    if (populate_VPVar(VP_M117, &ramcopy)) {
-      ramcopy.memadr = (void*) message;
-      if (ramcopy.send_to_display_handler) {
-        ramcopy.send_to_display_handler(ramcopy);
-      }
-    }
-
-    finishstatus(persist);
-  }
-
-  #include <stdarg.h>
-
-  void MarlinUI_DGUS::status_printf_P(const uint8_t level, PGM_P const fmt, ...) {
-
-    if (level < status_message_level) return;
-    va_list args;
-    va_start(args, fmt);
-    char status_message[VP_M117_LEN];
-    vsnprintf_P(status_message, VP_M117_LEN, fmt, args);
-    va_end(args);
-    status_message_level = 0;
-    set_status(status_message, level > 0);
-    status_message_level = level;
-  }
-
-  void MarlinUI_DGUS::set_status_P(PGM_P const message, int8_t level) {
-    if (level < 0) level = status_message_level = 0;
-    if (level < status_message_level) return;
-    status_message_level = level;
-
-    status_set = (pgm_read_byte(message) == '\0' ? false : true);
-
-    DGUS_VP_Variable ramcopy;
-    if (!populate_VPVar(VP_M117, &ramcopy)) return;
+  // The DGUS Display does not support UTF8, so we just pass it to it as it...
+  DGUS_VP_Variable ramcopy;
+  if (populate_VPVar(VP_M117, &ramcopy)) {
     ramcopy.memadr = (void*) message;
-    // we need to divert the handler here, as the one in the VP is the ram-based one.
-    DGUSScreenVariableHandler::DGUSLCD_SendStringToDisplayPGM(ramcopy);
-
-    finishstatus(level > 0);
+    if (ramcopy.send_to_display_handler) {
+      ramcopy.send_to_display_handler(ramcopy);
+    }
   }
 
-  void MarlinUI_DGUS::set_alert_status_P(PGM_P const message) {
-    set_status_P(message, 1);
-    ScreenHandler.GotoScreen(DGUSLCD_SCREEN_MAIN);  // or should that be a popup?
-  }
+  finishstatus(persist);
+}
 
-  #include "../../module/printcounter.h"
+#include <stdarg.h>
 
-  /**
-   * Reset the status message
-   */
-  void MarlinUI_DGUS::reset_status() {
-    static const char paused[] PROGMEM = MSG_PRINT_PAUSED;
-    static const char printing[] PROGMEM = MSG_PRINTING;
-    static const char welcome[] PROGMEM = WELCOME_MSG;
-    #if SERVICE_INTERVAL_1 > 0
-      static const char service1[] PROGMEM = { "> " SERVICE_NAME_1 "!" };
-    #endif
-    #if SERVICE_INTERVAL_2 > 0
-      static const char service2[] PROGMEM = { "> " SERVICE_NAME_2 "!" };
-    #endif
-    #if SERVICE_INTERVAL_3 > 0
-      static const char service3[] PROGMEM = { "> " SERVICE_NAME_3 "!" };
-    #endif
-    PGM_P msg;
-    if (!IS_SD_PRINTING() && print_job_timer.isPaused())
-      msg = paused;
-    #if ENABLED(SDSUPPORT)
-      else if (IS_SD_PRINTING())
-        return set_status(card.longest_filename(), true);
-    #endif
-    else if (print_job_timer.isRunning())
-      msg = printing;
+void MarlinUI_DGUS::status_printf_P(const uint8_t level, PGM_P const fmt, ...) {
 
-    #if SERVICE_INTERVAL_1 > 0
-      else if (print_job_timer.needsService(1)) msg = service1;
-    #endif
-    #if SERVICE_INTERVAL_2 > 0
-      else if (print_job_timer.needsService(2)) msg = service2;
-    #endif
-    #if SERVICE_INTERVAL_3 > 0
-      else if (print_job_timer.needsService(3)) msg = service3;
-    #endif
+  if (level < status_message_level) return;
+  va_list args;
+  va_start(args, fmt);
+  char status_message[VP_M117_LEN];
+  vsnprintf_P(status_message, VP_M117_LEN, fmt, args);
+  va_end(args);
+  status_message_level = 0;
+  set_status(status_message, level > 0);
+  status_message_level = level;
+}
 
-    else
-      msg = welcome;
+void MarlinUI_DGUS::set_status_P(PGM_P const message, int8_t level) {
+  if (level < 0) level = status_message_level = 0;
+  if (level < status_message_level) return;
+  status_message_level = level;
 
-    set_status_P(msg, -1);
-  }
+  status_set = (pgm_read_byte(message) == '\0' ? false : true);
 
-#endif // HAS_SPI_LCD
+  DGUS_VP_Variable ramcopy;
+  if (!populate_VPVar(VP_M117, &ramcopy)) return;
+  ramcopy.memadr = (void*) message;
+  // we need to divert the handler here, as the one in the VP is the ram-based one.
+  DGUSScreenVariableHandler::DGUSLCD_SendStringToDisplayPGM(ramcopy);
+
+  finishstatus(level > 0);
+}
+
+void MarlinUI_DGUS::set_alert_status_P(PGM_P const message) {
+  set_status_P(message, 1);
+  ScreenHandler.GotoScreen(DGUSLCD_SCREEN_MAIN);  // or should that be a popup?
+}
+
+#include "../../module/printcounter.h"
+
+/**
+ * Reset the status message
+ */
+void MarlinUI_DGUS::reset_status() {
+  static const char paused[] PROGMEM = MSG_PRINT_PAUSED;
+  static const char printing[] PROGMEM = MSG_PRINTING;
+  static const char welcome[] PROGMEM = WELCOME_MSG;
+  #if SERVICE_INTERVAL_1 > 0
+    static const char service1[] PROGMEM = { "> " SERVICE_NAME_1 "!" };
+  #endif
+  #if SERVICE_INTERVAL_2 > 0
+    static const char service2[] PROGMEM = { "> " SERVICE_NAME_2 "!" };
+  #endif
+  #if SERVICE_INTERVAL_3 > 0
+    static const char service3[] PROGMEM = { "> " SERVICE_NAME_3 "!" };
+  #endif
+  PGM_P msg;
+  if (!IS_SD_PRINTING() && print_job_timer.isPaused())
+    msg = paused;
+  #if ENABLED(SDSUPPORT)
+    else if (IS_SD_PRINTING())
+      return set_status(card.longest_filename(), true);
+  #endif
+  else if (print_job_timer.isRunning())
+    msg = printing;
+
+  #if SERVICE_INTERVAL_1 > 0
+    else if (print_job_timer.needsService(1)) msg = service1;
+  #endif
+  #if SERVICE_INTERVAL_2 > 0
+    else if (print_job_timer.needsService(2)) msg = service2;
+  #endif
+  #if SERVICE_INTERVAL_3 > 0
+    else if (print_job_timer.needsService(3)) msg = service3;
+  #endif
+
+  else
+    msg = welcome;
+
+  set_status_P(msg, -1);
+}
 
 #endif // DGUS_LCD
